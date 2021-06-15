b1cdc6c4e3e5751b5de18287717f7c46c04c3b44
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-14 20:42:09 -0500

    add get requests for first last and all contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,19 @@
 class ContactsController < ApplicationController
-  def show_contact
-    render json: {message: "hello"}
+  def show_all_contact
+    contact = Contact.all
+
+    render json: contact
+  end
+
+  def show_first_contact
+    contact1 = Contact.first
+
+    render json: contact1
+  end
+
+  def show_last_contact
+    contact3 = Contact.last
+
+    render json: contact3
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/one_contact", controller: "contacts", action: "show_contact"
+  get "/one_contact", controller: "contacts", action: "show_last_contact"
+  get "/one_contact", controller: "contacts", action: "show_first_contact"
+  get "/one_contact", controller: "contacts", action: "show_all_contact"
 end
