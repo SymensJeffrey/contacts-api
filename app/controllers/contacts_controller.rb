9f94cee3dd4f7744532e356498634aa0a31ff56b
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-22 21:27:11 -0500

    add address search

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,18 @@ class ContactsController < ApplicationController
   end
 
   def create
-
-    render json: {message: "hellp"}
+    results = Geocoder.search(params[:address])
+    latitude = results.first.coordinates[0]
+    longitude = results.first.coordinates[1]
+    contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      latitude:latitude,
+      longitude: longitude
+    )
+    contact.save
+    render json: contact.as_json
   end
 end
