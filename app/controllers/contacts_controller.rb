22cd350af5c1882f2ff77d2d550ff4bf76e942a6
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-22 20:14:05 -0500

    add index

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,5 @@
 class ContactsController < ApplicationController
-  def show_all_contact
-    contact = Contact.all
-
-    render json: contact
-  end
-
-  def show_first_contact
-    contact1 = Contact.first
-
-    render json: contact1
-  end
-
-  def show_last_contact
-    contact3 = Contact.last
-
-    render json: contact3
-  end
-
-  def show_second_contact
-    contact2 = Contact.second 
-
-    render json: contact2
+  def index
+    render json: {message: "hello"}
   end
 end
